99385f8d91014531912215310ebcfde9d4debdc3
Yulia Kits <volokolamskspb@gmail.com>
2025-01-15 17:22:08 +0200

    Refactor: movie preview

--- src/entities/Movie/ui/MoviePreview/MoviePreview.tsx
+++ src/entities/Movie/ui/MoviePreview/MoviePreview.tsx
@@ -2,15 +2,16 @@ import { Link } from 'react-router-dom';
 import { MovieThumbnail } from '@/entities/Movie';
 import { MovieItemProps } from '@/entities/Movie/types';
 import { IMAGE_URL } from '@/shared/apiConstants';
+import './MoviePreview.scss';
 
 const MoviePreview: React.FC<MovieItemProps> = ({ movie }) => (
-  <Link to={`/movie/${movie.id}`} key={movie.id} className="movie-list__movie-item">
+  <Link to={`/movie/${movie.id}`} key={movie.id} className="movie-preview__movie-item">
     <MovieThumbnail 
       imageUrl={`${IMAGE_URL}w200${movie.poster_path}`} 
       title={movie.title}
-      className='movie-list__thumbnail'
+      className='movie-preview__thumbnail'
     />
-    <p className="movie-list__movie-title">{movie.title}</p>
+    <p className="movie-preview__title">{movie.title}</p>
   </Link>
 );
 
